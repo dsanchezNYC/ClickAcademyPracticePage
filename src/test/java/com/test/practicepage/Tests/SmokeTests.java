51a7780e08073186432583e531c08fa85a5af0f9
Daniel Sanchez <daniel.sanchez@silverlinecrm.com>
2019-09-14 21:02:27 -0400

    Update to run via remote ChromeDriver

--- src/test/java/com/test/practicepage/Tests/SmokeTests.java
+++ src/test/java/com/test/practicepage/Tests/SmokeTests.java
@@ -20,6 +20,7 @@ public class SmokeTests extends BaseTests {
             $(pp.PracticePageHeader).should(Condition.exist);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Navigate To Project Page' test ***PASSED***");
     }
@@ -32,6 +33,7 @@ public class SmokeTests extends BaseTests {
             $(hp.practiceLink).should(Condition.exist);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Navigate To Home Page' test ***PASSED***");
     }
@@ -45,6 +47,7 @@ public class SmokeTests extends BaseTests {
             $(pp.RadioButton1).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Radio button 1' test ***PASSED***");
     }
@@ -58,6 +61,7 @@ public class SmokeTests extends BaseTests {
             $(pp.RadioButton2).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Radio button 2' test ***PASSED***");
     }
@@ -71,6 +75,7 @@ public class SmokeTests extends BaseTests {
             $(pp.RadioButton3).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Radio button 3' test ***PASSED***");
     }
@@ -84,6 +89,7 @@ public class SmokeTests extends BaseTests {
             $(pp.CheckboxOption1).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Checkbox option 1' test ***PASSED***");
     }
@@ -97,6 +103,7 @@ public class SmokeTests extends BaseTests {
             $(pp.CheckboxOption2).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Checkbox option 2' test ***PASSED***");
     }
@@ -110,6 +117,7 @@ public class SmokeTests extends BaseTests {
             $(pp.CheckboxOption3).shouldBe(Condition.focused);
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Checkbox option 3' test ***PASSED***");
     }
@@ -123,6 +131,7 @@ public class SmokeTests extends BaseTests {
             $(pp.DropdownExample.shouldHave(value("option1")));
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Dropdown option 1' test ***PASSED***");
     }
@@ -136,6 +145,7 @@ public class SmokeTests extends BaseTests {
             $(pp.DropdownExample.shouldHave(value("option2")));
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Dropdown option 2' test ***PASSED***");
     }
@@ -149,6 +159,7 @@ public class SmokeTests extends BaseTests {
             $(pp.DropdownExample.shouldHave(value("option3")));
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select Dropdown option 3' test ***PASSED***");
     }
@@ -163,6 +174,7 @@ public class SmokeTests extends BaseTests {
             $(pp.SelectCountries.shouldHave(value("United States")));
         }
         catch(Exception e){
+            e.printStackTrace();
         }
         System.out.println("[INFO]: 'Select United States from Select Countries' test ***PASSED***");
     }
